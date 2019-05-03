5b2cec20fede8d5d4326bc5e3eb71846c8253174
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2019-05-03 03:48:09 -0400

    Write the whole algorithm in sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,63 @@
 let looping = true;
 let socket, cnvs, ctx, canvasDOM;
-let fileName = "./frames/sketch";
-let maxFrames = 20;
+let fileName = "./frames/4/sketch";
+let maxFrames = 126;
+let x, y, t;
+let r = 100;
+let increment;
+let amount = 1500;
 
 function setup() {
+    // pixelDensity(1);
     socket = io.connect('http://localhost:8080');
-    cnvs = createCanvas(windowWidth, windowWidth / 16 * 9);
+    // cnvs = createCanvas(windowWidth, windowWidth / 16 * 9);
+    cnvs = createCanvas(500, 500);
     ctx = cnvs.drawingContext;
     canvasDOM = document.getElementById('defaultCanvas0');
+    frameRate(5);
+    background(230);
+    fill(0);
+    // noStroke();
+    stroke(50, 0, 15);
+    stroke(0);
     frameRate(30);
-    background(0);
-    fill(255, 50);
-    noStroke();
+    strokeWeight(1);
     if (!looping) {
         noLoop();
     }
 }
 
 function draw() {
-    for (let i = 0; i < 500; i++) {
-        let x = random(width);
-        let y = random(height);
-        ellipse(x, y, 5);
+    // background(150, 250, 190);
+    // background(230);
+    background(255);
+    increment = TWO_PI / amount;
+    t = frameCount;
+    let k = 0;
+    let curve = map(sin(frameCount * 0.05), -1, 1, 0.95, 1.05);
+    for (let i = 0; i < TWO_PI - increment; i += increment) {
+        r = map(sin(i * 5), -1, 1, curve, 1) * 50;
+        x = cos(i) * r + 250;
+        y = sin(i) * r + 250;
+        // y = x * x;
+        if (k % 10 == 0) {
+            let j = i + increment;
+            let r2 = map(sin(j * 5), -1, 1, curve, 1) * 50;
+            let x2 = cos(j) * r2 + 250;
+            let y2 = sin(j) * r2 + 250;
+            // y2 = 
+            // ellipse(x, y, 10);
+            // stroke(0);
+            let slope = (y2 - y) / (x2 - x);
+            let x0 = x - 1000;
+            let x3 = x2 + 1000;
+            let y0 = y - 1000 * slope;
+            let y3 = y2 + 1000 * slope;
+            line(x0, y0, x3, y3);
+            // noStroke();
+        }
+        // ellipse(x, y, 2);
+        k++;
     }
     if (exporting && frameCount < maxFrames) {
         frameExport();
